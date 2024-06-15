9a75a7eba9b193507650a41dcc2a0aa722e25aec
Rin <okarin112@mail.ru>
2024-06-15 12:17:16 +0800

    Домашнее задание

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-a = 'Hello Word'
-print(a)
-b = 3
+import requests
+
+
+url = 'https://fakestoreapi.com/products'
+response = requests.get(url).json()
+#pprint.pprint(response)
+
+d = json.dumps(response, indent=4)
+
+data = input('Какой')
+response = requests.get(url).json()
+flag = False
+for i in response:
+    if i ['name']['lastname'] == data:
+       print(f'Имя: {i["name"]["firstname"]}, Фамилия: {i["name"]["lastname"]} ')
+       print(f'Телефон: {i["phone"]}, Email: {i["email"]}')
+       flag = True
+if not flag:
+    print('Нет')
+
+
