0b84e575bf6acd55e40d12e0aafc2ec5fd7773a5
Rin <okarin112@mail.ru>
2024-06-15 12:17:44 +0800

    Задание

--- test.py
+++ test.py
@@ -1,21 +1,12 @@
 import requests
-
+import json
 
 url = 'https://fakestoreapi.com/products'
 response = requests.get(url).json()
 #pprint.pprint(response)
 
-d = json.dumps(response, indent=4)
+d = json.dumps(response)
 
-data = input('Какой')
-response = requests.get(url).json()
-flag = False
-for i in response:
-    if i ['name']['lastname'] == data:
-       print(f'Имя: {i["name"]["firstname"]}, Фамилия: {i["name"]["lastname"]} ')
-       print(f'Телефон: {i["phone"]}, Email: {i["email"]}')
-       flag = True
-if not flag:
-    print('Нет')
+print(d[1])
 
 
