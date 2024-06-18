bc943b7b1891fd89d482cb5a81174f164efe671c
Rin <okarin112@mail.ru>
2024-06-18 12:06:47 +0800

    Задание 3.1

--- test.py
+++ test.py
@@ -4,24 +4,23 @@ import json
 
 url = "https://fakestoreapi.com/products/categories"
 response = requests.get(url).json()
-print(response)
+
 print('Список категорию товаров: ')
-for i in response:
-    print(i.title())
-
-
-def tovar(a):
-    flag = False
-    for i in response:
-        if i == a:
-            print('Вы выбрали:',a.title())
-            flag = True
-    if not flag:
-        print('Нет такой категории товара')    
-
+print(', '.join(response))
 
 a = input('Введите категорию товара: ')
-b = tovar(a)
+
+flag = False
+for i in response:
+    if i == a:
+         print('Вы выбрали:',a.title())
+         flag = True
+if not flag:
+    print('Нет такой категории товара')    
+
+
+
+
 
 
 
