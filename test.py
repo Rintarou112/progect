55459cc3a5b07c685878461e469f69ba93605d20
Rin <okarin112@mail.ru>
2024-06-18 11:57:00 +0800

    Задание 3.1

--- test.py
+++ test.py
@@ -7,13 +7,17 @@ response = requests.get(url).json()
 print(response)
 print('Список категорию товаров: ')
 for i in response:
-    print(i)
+    print(i.title())
+
 
 def tovar(a):
+    flag = False
     for i in response:
         if i == a:
             print('Вы выбрали:',a.title())
-        
+            flag = True
+    if not flag:
+        print('Нет такой категории товара')    
 
 
 a = input('Введите категорию товара: ')
